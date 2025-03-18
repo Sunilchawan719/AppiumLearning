97a51bbbbc076bf9dde88b1d615032973c4814ff
Sunilchawan <sunil.chawan@healthgraph.in>
2025-03-18 15:54:46 +0530

    first commit

---
+++ src/test/java/AppiumBasics.java
@@ -0,0 +1,49 @@
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class AppiumBasics {
+
+    @Test
+    public void appiumTest() throws IOException, URISyntaxException {
+
+        // Set up Android Driver
+        //Appium Code > Appium Server > Mobile
+
+        //To Start Appium Service
+        /* AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\Sunil\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
+        .withIPAddress("127.0.0.1").usingPort(4723).build();
+        service.start(); */
+
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("Pixel 4");   //Device Name - RZCWA12A1BJ, samsung SM-A346E
+        options.setApp("C:\\Users\\Sunil\\Downloads\\QATestApp-1.1.apk");
+        options.setAutomationName("UiAutomator2");
+
+        AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+        System.out.println("Appium Driver is Successfully initialized");
+
+        // Perform test actions
+        //Appium Will Support Only Xpath, ID, ClassName, AccessibilityId , AndroidUIAutomator
+        driver.findElement(By.id("com.qatestapp:id/emailTextField1")).sendKeys("helloworld@gmail.com");
+        driver.findElement(AppiumBy.id("com.qatestapp:id/passwordTextField")).sendKeys("testing1234");
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.qatestapp:id/LoginButton\"]")).click();
+
+
+        // Close the driver properly
+     //   driver.quit();
+      //  service.stop();
+
+    }
+}
